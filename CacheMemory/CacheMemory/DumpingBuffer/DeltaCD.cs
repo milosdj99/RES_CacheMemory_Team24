9fac98eb00cd28616837685e575d5eccaa818979
milosdj99 <milosdj9999@gmail.com>
2021-05-21 16:42:58 +0200

    Dodate klase

---
+++ CacheMemory/CacheMemory/DumpingBuffer/DeltaCD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheMemory.DumpingBuffer
+{
+    class DeltaCD
+    {       
+
+        int TransactionId { get; set; }
+
+        CollectionDescription Add { get; set; }
+
+        CollectionDescription Update { get; set; }
+
+        CollectionDescription Remove { get; set; }
+
+        public DeltaCD(int transactionId, CollectionDescription add, CollectionDescription update, CollectionDescription remove)
+        {
+            TransactionId = transactionId;
+            Add = add;
+            Update = update;
+            Remove = remove;
+        }
+    }
+}
