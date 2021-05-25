eb9a57489c67fc0081c0501d0fe9794c15a120dd
milosdj99 <milosdj9999@gmail.com>
2021-05-25 16:28:35 +0200

    reader logika

--- CacheMemory/CacheMemory/Reader/Reader.cs
+++ CacheMemory/CacheMemory/Reader/Reader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CacheMemory.Historical;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,44 @@ namespace CacheMemory.Reader
 {
     class Reader
     {
-        
+        public Historical.Historical Historical { get; set; }
+
+        public Reader(Historical.Historical historical)
+        {
+            Historical = historical;
+        }
+
+        public void DataSetSearch(int dataset)
+        {
+            List<HistoricalProperty> returnList = Historical.DataSetSearch(dataset);
+
+            Console.WriteLine("Code: ||| Id geografskog podrucja: ||| Timestamp: ||| Vrednost: \n");
+            foreach (HistoricalProperty hs in returnList)
+            {
+                Console.WriteLine($"{hs.Code} ||| {hs.Value.IdGeografskogPodrucja} ||| {hs.Value.TimeStamp}: ||| {hs.Value.Potrosnja} ");
+            }
+        }
+
+        public void CodeSearch(string code)
+        {
+            List<HistoricalProperty> returnList = Historical.CodeSearch(code);
+
+            Console.WriteLine("Code: ||| Id geografskog podrucja: ||| Timestamp: ||| Vrednost: \n");
+            foreach (HistoricalProperty hs in returnList)
+            {
+                Console.WriteLine($"{hs.Code} ||| {hs.Value.IdGeografskogPodrucja} ||| {hs.Value.TimeStamp}: ||| {hs.Value.Potrosnja} ");
+            }
+        }
+
+        public void DateSearch(DateTime dateMin, DateTime dateMax)
+        {
+            List<HistoricalProperty> returnList = Historical.DateSearch(dateMin, dateMax);
+
+            Console.WriteLine("Code: ||| Id geografskog podrucja: ||| Timestamp: ||| Vrednost: \n");
+            foreach (HistoricalProperty hs in returnList)
+            {
+                Console.WriteLine($"{hs.Code} ||| {hs.Value.IdGeografskogPodrucja} ||| {hs.Value.TimeStamp}: ||| {hs.Value.Potrosnja} ");
+            }
+        }
     }
 }
